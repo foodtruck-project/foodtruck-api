4b18fca37a742172516bc54494117829eea81181
bento <bentoluizv@gmail.com>
2025-09-07 11:30:36 -0300

    refactor: streamline user service dependency injection and convert setup function to async

--- projeto_aplicado/resources/setup/controller.py
+++ projeto_aplicado/resources/setup/controller.py
@@ -2,29 +2,30 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-from projeto_aplicado.resources.user.repository import (
-    UserRepository,
-    get_user_repository,
+from projeto_aplicado.resources.user.service import (
+    UserService,
+    get_user_service,
 )
-from projeto_aplicado.resources.user.service import UserService
 from projeto_aplicado.settings import get_settings
 
 settings = get_settings()
-
-UserRepo = Annotated[UserRepository, Depends(get_user_repository)]
-
-
-def get_user_service(user_repo: UserRepo) -> UserService:
-    return UserService(repository=user_repo)
 
 
 UserServiceDep = Annotated[
     UserService,
     Depends(get_user_service),
 ]
-router = APIRouter(tags=['Setup'], prefix=f'{settings.API_PREFIX}/setup')
+
+
+router = APIRouter(
+    tags=['Setup'],
+    prefix=f'{settings.API_PREFIX}/setup',
+    include_in_schema=False,
+)
 
 
 @router.get('/setup', status_code=200)
-def setup_app(user_service: UserServiceDep):
-    user_service.create_default_users()
+async def setup_app(
+    user_service: UserServiceDep,
+):
+    await user_service.create_default_users()
